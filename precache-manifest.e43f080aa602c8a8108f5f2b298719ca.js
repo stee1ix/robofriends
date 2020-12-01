d1c33ce3aa11208c690bf4fcfc0c43af465df743
stee1ix <rituraj.ranjan.42@gmail.com>
2020-12-01 15:17:07 +0530

    Updates

---
+++ precache-manifest.e43f080aa602c8a8108f5f2b298719ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ac4c2d20df76212c69db70c2dba5dbd",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "0556eed029c159efc285",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2a5f68062988f9ca4857",
+    "url": "/robofriends/static/css/main.16b1e22e.chunk.css"
+  },
+  {
+    "revision": "0556eed029c159efc285",
+    "url": "/robofriends/static/js/2.8fa0d876.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/robofriends/static/js/2.8fa0d876.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a5f68062988f9ca4857",
+    "url": "/robofriends/static/js/main.77a03b7f.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
